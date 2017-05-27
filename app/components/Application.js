7d0a1c1894110ea731513ea9234fe4db1693944f
Steve Kinney <hello@stevekinney.net>
2017-05-27 14:14:38 -0600

    Get basic functionality working

--- app/components/Application.js
+++ app/components/Application.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NewItem from './NewItem';
+import Item from './Item';
 
 class Application extends Component {
   constructor(props) {
@@ -7,21 +8,44 @@ class Application extends Component {
     this.state = {
       items: []
     };
+
+    this.markAsPacked = this.markAsPacked.bind(this);
+  }
+
+  markAsPacked(item) {
+    const otherItems = this.state.items.filter(other => other.id !== item.id);
+    const updatedItem = { ...item, packed: !item.packed };
+    console.log(item, updatedItem, otherItems);
+    this.setState({ items: [updatedItem, ...otherItems] });
   }
 
   render() {
     const { items } = this.state;
-    console.log(items);
+    const unpackedItems = items.filter(item => !item.packed);
+    const packedItems = items.filter(item => item.packed);
 
     return (
       <div className="Application">
-        <NewItem
-          onSubmit={item => this.setState({ items: [...items, item] })}
-        />
+        <NewItem onSubmit={this.addItem} />
         <section className="Items">
-          {items.map(item => {
-            return <article className="Item" key={item.id}>{item.value}</article>;
-          })}
+          <h2>Unpacked Items</h2>
+          {unpackedItems.map(item => (
+            <Item
+              key={item.id}
+              onCheckOff={() => this.markAsPacked(item)}
+              {...item}
+            />
+          ))}
+        </section>
+        <section className="Items">
+          <h2>Packed Items</h2>
+          {packedItems.map(item => (
+            <Item
+              key={item.id}
+              onCheckOff={() => this.markAsPacked(item)}
+              {...item}
+            />
+          ))}
         </section>
       </div>
     );
--- app/components/NewItem.js
+++ app/components/NewItem.js
@@ -21,7 +21,7 @@ class NewItem extends Component {
     const { value } = this.state;
 
     event.preventDefault();
-    onSubmit({ value, id: Date.now() });
+    onSubmit({ value, packed: false, id: Date.now() });
     this.setState({ value: '' });
   }
 
