5283b04cb9510a156e6aa4426f9488b46a7b9b94
Steve Kinney <hello@stevekinney.net>
2017-05-27 13:36:10 -0600

    Render items

--- app/components/Application.js
+++ app/components/Application.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 import NewItem from './NewItem';
 
 class Application extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+  }
 
   render() {
+    const { items } = this.state;
+    console.log(items);
+
     return (
       <div className="Application">
-        <NewItem />
+        <NewItem
+          onSubmit={item => this.setState({ items: [...items, item] })}
+        />
+        <section className="Items">
+          {items.map(item => {
+            return <article className="Item" key={item.id}>{item.value}</article>;
+          })}
+        </section>
       </div>
     );
   }
