3fb1cde4644dc387f11e51cdde7919e93bfd8e2e
Steve Kinney <hello@stevekinney.net>
2017-05-27 12:59:27 -0600

    Get the basic page up and running

--- app/main.js
+++ app/main.js
@@ -1,5 +1,11 @@
-import { app } from 'electron';
+import { app, BrowserWindow } from 'electron';
+
+let mainWindow;
 
 app.on('ready', () => {
-  console.log('The application is ready.');
+  mainWindow = new BrowserWindow({ width: 300, height: 600, show: false });
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
 });
