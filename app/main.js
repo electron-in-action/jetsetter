0242ccf892728be982bb2d21b692ae524849e64c
Steve Kinney <hello@stevekinney.net>
2017-06-25 14:42:37 -0600

    Get babel working

--- app/main.js
+++ app/main.js
@@ -3,7 +3,7 @@ import { app, BrowserWindow } from 'electron';
 let mainWindow;
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({ width: 300, height: 600, show: false });
+  mainWindow = new BrowserWindow({ width: 300, height: 600, minWidth: 300, show: false });
   mainWindow.loadURL(`file://${__dirname}/index.html`);
   mainWindow.once('ready-to-show', () => {
     mainWindow.show();
