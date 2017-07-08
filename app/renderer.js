544bb2d4e65a4f0a66fd9fce22ecb250cab81373
Steve Kinney <hello@stevekinney.net>
2017-07-08 12:05:29 -0600

    Make a placeholder <Application />

--- app/renderer.js
+++ app/renderer.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Application from './components/Application';
+// import Application from './components/Application';
+
+const Application = () => {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <button className="full-width">
+        This button doesn't do anything.
+      </button>
+    </div>
+  );
+};
 
 render(<Application />, document.getElementById('application'));
