70a2e5103ab10d65844bbb178fd68a59f48257d9
Steve Kinney <hello@stevekinney.net>
2017-08-12 09:56:16 -0700

    Use async/await

--- app/renderer.js
+++ app/renderer.js
@@ -4,8 +4,7 @@ import { AppContainer } from 'react-hot-loader';
 import Application from './components/Application';
 
 const renderApplication = async () => {
-  const Application = require('./components/Application').default;
-  // const { default: Application } = await import('./components/Application');
+  const { default: Application } = await import('./components/Application');
   render(
     <AppContainer>
       <Application />
