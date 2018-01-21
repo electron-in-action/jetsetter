d860fb54f2142452bc95f4e2b019d6f011205a38
Steve Kinney <hello@stevekinney.net>
2018-01-21 14:03:15 -0700

    Use a label tag

--- app/components/Item.js
+++ app/components/Item.js
@@ -3,8 +3,10 @@ import React from 'react';
 const Item = (({ packed, id, value, onCheckOff }) => {
   return (
     <article className="Item">
+      <label>
       <input type="checkbox" checked={packed} onChange={onCheckOff} />
-      <span>{value}</span>
+      {value}
+      </label>
     </article>
   );
 });
